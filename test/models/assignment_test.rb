1a0327d81a02f1dac65c831ab5f9fd09f6bc5db6
Anna Bedell <awbedell@gmail.com>
2015-06-26 10:18:19 -0400

    assignment average test

--- test/models/assignment_test.rb
+++ test/models/assignment_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class AssignmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "average" do
+    teacher = teachers(:one)
+    assignment = Assignment.create(name: "test", teacher_id: teacher.id, due: Date.today)
+    Grade.create(student_id: students(:one).id, assignment_id: assignment.id, score: 100)
+    Grade.create(student_id: students(:two).id, assignment_id: assignment.id, score: 90)
+    Grade.create(student_id: students(:three).id, assignment_id: assignment.id)
+
+    assert_equal 95, assignment.average
+  end
 end
