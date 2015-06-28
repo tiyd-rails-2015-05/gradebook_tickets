dca33e3ee4a91141fbe1e7aaeb6e123cf1a25636
Scott Williams <williamsjs1224@gmail.com>
2015-06-28 19:59:24 -0400

    write method for incomplete achievements

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,19 @@ class Student < ActiveRecord::Base
   has_many :parents
   belongs_to :teacher
   has_secure_password
+
+  def incomplete_achievements
+    incomplete = []
+    completed = achievements.map {|a| a.id}
+    all_achievements = Achievement.all
+    all_achievements.each do |a|
+      unless completed.include?(a.id)
+        incomplete << a
+      end
+    end
+    incomplete
+  end
+
+
+
 end
